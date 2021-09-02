03791bd4c7bb9eb4bf63b122ab3ec563f9739079
xacwolf <62719306+xacwolf@users.noreply.github.com>
2021-09-03 01:30:17 +0300

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -1,1 +1,17 @@
-
+#!/bin/sh 
+if
+PPTP=`ip ro | awk '/172.0.0.0/ {print $1}'`;
+test "$PPTP" = "172.0.0.0/24"
+then
+exit;
+else
+if
+PPTPup=`ip ro | awk '/10.0.0.1/ {print $1}'`;
+test "$PPTPup" != ""
+then
+route add -net 172.0.0.0 netmask 255.255.255.0 gw 10.0.0.1
+else
+exit;
+fi
+fi
+exit;
